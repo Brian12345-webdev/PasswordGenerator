3831181dc318d27f14eff0e0298638a335ae9399
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-07 15:39:58 -0500

    update to js

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,30 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// I am not sure why the queryselector is there since there is no "#generate" in CSS ask tutor
+// var generateBtn = document.querySelector("#generate");
+var length = Number(prompt("Enter a number between 8 and 128")),
+    charType = prompt("Enter a character type: special, numberic, uppercase, lowercase"),
+    password = writePassword();
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var charset = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    number: "0123456789",
+    special: '~!@#$%^&*()_+{}|:"<>?`-=[]\;,./"'
+    // var password = generatePassword();
+    // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    //passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Copy Text button
+// function copyPassword() {
+//     var copyText = document.getElementById("password");
+//     copyText.select();
+//     document.execCommand("Copy Password");
+// }
