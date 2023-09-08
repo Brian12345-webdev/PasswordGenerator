a7ae2868ae904440985d0f193f3487db15ac5d96
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-07 21:20:20 -0500

    refresh given code, finished prompts, stuck at return due to writePassword function is undefined

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,17 @@
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = prompt("Please enter a password length between 8 and 128 characters");
+  if (passwordLength < 8 || passwordLength > 128) 
+    alert("Please enter a password length between 8 and 128 characters");
+  var Numbers = prompt("Would you like to use numbers in your password?");
+  var Lowercase = prompt("Would you like to use lowercase letters in your password?");
+  var Uppercase = prompt("Would you like to use uppercase letters in your password?");
+  var SpecialCharacters = prompt("Would you like to use special characters in your password?");
+  return; writePassword//this is where I am stuck, The write password function is undefined.
+  
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,16 +23,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-// Prompts
-function generatePassword(){
-    var passwordLength = prompt("Please enter the number of characters you want between 8 and 128")
-    var lowercase = prompt("Do you want to include lowercase letters in your password?")
-    var uppercase = prompt("Do you want to include uppercase letters in your password?")
-    var special = prompt("Do you want to include special characters in your password?")
-    var numbers = prompt("Do you want to include numbers in your password?")
-}
-
-
-
