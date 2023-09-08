1a5ee31f19a59e9aa131db88948af2f0b23da77d
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-08 14:03:29 -0500

    update to js with randomselction using floor and random math

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,36 @@ function generatePassword() {
   var passwordLength = prompt("Please enter a password length between 8 and 128 characters");
   if (passwordLength < 8 || passwordLength > 128) 
     alert("Please enter a password length between 8 and 128 characters");
-  var Numbers = prompt("Would you like to use numbers in your password?");
-  var Lowercase = prompt("Would you like to use lowercase letters in your password?");
-  var Uppercase = prompt("Would you like to use uppercase letters in your password?");
-  var SpecialCharacters = prompt("Would you like to use special characters in your password?");
-  return; writePassword//this is where I am stuck, The write password function is undefined.
+  var numbers = confirm("Would you like to use numbers in your password?");
+  var lowercase = confirm("Would you like to use lowercase letters in your password?");
+  var uppercase = confirm("Would you like to use uppercase letters in your password?");
+  var specialCharacters = confirm("Would you like to use special characters in your password?");
+  
+  // store a string of possible user choices, consolidate in to one big string
+  var possibleCharacters = "";
+  if (numbers) {
+    possibleCharacters += "0123456789"
+  }
+  if (lowercase) {
+    possibleCharacters += "abcdefghijklmnopqrstuvwxyz"
+  }
+  if (uppercase) {
+    possibleCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  if (specialCharacters) {
+    possibleCharacters += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+  }
+  
+var bucketCharacters = "";
+  for (var i = 0; i < possibleCharacters.length; i++)
+  {
+    if (bucketCharacters.length < passwordLength)
+    {
+      bucketCharacters += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+    }
+  }
+
+return bucketCharacters; 
   
 }
 // Write password to the #password input
