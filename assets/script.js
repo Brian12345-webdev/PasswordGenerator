e575100aff31dab6e32bb71d325a154b5ef0873c
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-07 19:33:15 -0500

    update of prompts

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,26 @@
-// I am not sure why the queryselector is there since there is no "#generate" in CSS ask tutor
-// var generateBtn = document.querySelector("#generate");
-var length = Number(prompt("Enter a number between 8 and 128")),
-    charType = prompt("Enter a character type: special, numberic, uppercase, lowercase"),
-    password = writePassword();
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var charset = {
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    number: "0123456789",
-    special: '~!@#$%^&*()_+{}|:"<>?`-=[]\;,./"'
-    // var password = generatePassword();
-    // var passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-    //passwordText.value = password;
-  }
+  passwordText.value = password;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// Copy Text button
-// function copyPassword() {
-//     var copyText = document.getElementById("password");
-//     copyText.select();
-//     document.execCommand("Copy Password");
-// }
+// Prompts
+function generatePassword(){
+    var passwordLength = prompt("Please enter the number of characters you want between 8 and 128")
+    var lowercase = prompt("Do you want to include lowercase letters in your password?")
+    var uppercase = prompt("Do you want to include uppercase letters in your password?")
+    var special = prompt("Do you want to include special characters in your password?")
+    var numbers = prompt("Do you want to include numbers in your password?")
+}
+
+
+
